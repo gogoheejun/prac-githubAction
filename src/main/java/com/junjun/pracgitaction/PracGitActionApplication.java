ec28804e31ff82588ed7cb88e11c933ecdfaa694
heejun <heejjuunn@gmail.com>
2021-11-14 14:09:47 +0900

    feat: base controller

---
+++ src/main/java/com/junjun/pracgitaction/PracGitActionApplication.java
@@ -0,0 +1,13 @@
+package com.junjun.pracgitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracGitActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracGitActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junjun/pracgitaction/PracGitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junjun.pracgitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracGitActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
